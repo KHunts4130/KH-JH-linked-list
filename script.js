211a6634b207ef3a39f6f7e1b8d2e43cd0f4db52
Justin <jhighland99@gmail.com>
2018-05-12 14:05:45 -0600

    Enable button working js

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
 var $cardTitle = $('.title-input-field');
-var $cardList = $('.card-list');
 var $cardURL = $('.main-URL-input-field');
 var $enterButton = $('.enter-button');
+var $cardList = $('.card-list');
 
-$cardList.on('click', deleteCard);
-$cardList.on('click', markAsRead);
-$enterButton.on('click', emptyInputField);
+$enterButton.on('click', makeCard);
+$cardTitle.on('click', enablesButton);
+$cardURL.on('click', enablesButton);
 
+function makeCard() {
+  console.log('makeCard');
+}
 
-function appendCard() {
-
-};
-
-
-function emptyInputField() {
-  if(cardTitle.val('') || cardURL.val('')){
-    alert('Error, emtpy input fields');
+function enablesButton() {
+  console.log('enable buttons');
+  if ($cardTitle.val() !== '' && $cardURL.val() !== '') {
+    $enterButton.prop('disabled', false)
   } else {
-    appendCard();
+    $enterButton.prop('disabled', true);
   }
 };
-
-
