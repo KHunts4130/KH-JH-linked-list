18e4cbe1e165ae6bc733ccb69f3e5293973eef25
Kent Huntsman <kenthuntsman@yahoo.com>
2018-05-15 14:06:24 -0600

    on click of read button the class of read is added to the li

--- script.js
+++ script.js
@@ -4,30 +4,34 @@ var $cardTitle = $('.title-input-field');
 var $cardURL = $('.main-URL-input-field');
 var $enterButton = $('.enter-button');
 var $cardList = $('.card-list');
+var $removeReadCards = $('.remove-read-cards')
 
 // Event listeners
 
 $enterButton.on('click', doesCardGetMade);
 $cardList.on('click', deleteCard);
-$cardList.on('click', markRead);
+$removeReadCards.on('click', removeReadCards);
+
 // functions
 
 function makeCard() {
   var createCard = $('<li>');
-  var cardURL = 'https://' + $cardURL.val();
+  var cardURL = $cardURL.val();
   var cardTitle = $cardTitle.val();
  createCard.html(`
     <h3 class="main-right-card-title">${cardTitle}</h3>
-    <a target="_blank" class="card-link" href="${cardURL}">${$cardURL.val()}</a>
+    <a target="_blank" class="card-link" href="${'https://' + cardURL}">${cardURL}</a>
     <button class="card-button card-button-read" >Read</button>
     <button class="card-button card-button-delete">Delete</button>
     `);
   $cardList.append(createCard); 
+  var $readButton = $('.card-button-read');
+  $readButton.on('click', markRead)
   clearInputFields();
 }
 
 function checkTitleField() {
-  titleCheck = $cardTitle.val() || 'Enter a title and' 
+  titleCheck = $cardTitle.val() || 'enter a title' 
 }
 
 function checkURLField() {
@@ -50,12 +54,21 @@ function doesCardGetMade() {
 };
 
 function deleteCard(event) {
-  if (event.target.matches('.card-button-delete')){
+  if (event.target.matches('.card-button-delete')) {
   event.target.closest('li').remove();
   }
 }
 
-function markRead() {
-  console.log("markRead function");
+function markRead(event) {
+  if (event.target.matches('.card-button-read')) {
+    $(this).parent().toggleClass('read');
+  } 
 }
+
+function removeReadCards() {
+  $('.read').remove();
+}
+
+
+
 
