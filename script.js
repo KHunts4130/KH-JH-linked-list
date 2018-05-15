56f5b6fe90bc3ab137e99de4d964a9738bff809a
Kent Huntsman <kenthuntsman@yahoo.com>
2018-05-14 20:51:03 -0600

    Check input fields for content functions

--- script.js
+++ script.js
@@ -1,30 +1,57 @@
+// Variables
+
 var $cardTitle = $('.title-input-field');
 var $cardURL = $('.main-URL-input-field');
 var $enterButton = $('.enter-button');
 var $cardList = $('.card-list');
 
-$enterButton.on('click', makeCard);
-$cardTitle.on('keyup', enablesButton);
-$cardURL.on('keyup', enablesButton);
+// Event listeners
+
+$enterButton.on('click', doesCardGetMade);
+
+// functions
 
 function makeCard() {
-  console.log('makeCard function');
   var createCard = $('<li>');
   createCard.html(`
     <h3 class="main-right-card-title">${$cardTitle.val()}</h3>
-  <a class="card-link" href="">${$cardURL.val()}</a>
-  <button class="card-button card-button-read">Read</button>
-  <button class="card-button card-button-delete">Delete</button>
-  `);
+    <a class="card-link" href="">${$cardURL.val()}</a>
+    <button class="card-button card-button-read">Read</button>
+    <button class="card-button card-button-delete">Delete</button>
+    `);
   $cardList.append(createCard); 
+  clearInputFields();
 }
 
-function enablesButton() {
-  console.log('enable buttons');
-  if ($cardTitle.val() !== '' && $cardURL.val() !== '') {
-    $enterButton.prop('disabled', false)
+function checkTitleField() {
+  titleCheck = $cardTitle.val() || 'enter a title' 
+}
+
+function checkURLField() {
+  UrlCheck = $cardURL.val() || 'enter a web site'
+}
+
+function clearInputFields() {
+  $cardTitle.val('');
+  $cardURL.val('');
+}
+
+function doesCardGetMade() {
+  checkTitleField();
+  checkURLField();
+  if ($cardTitle.val() === '' || $cardURL.val() === '') {
+    alert(titleCheck + ' ' + UrlCheck)
+  } else {
+  makeCard(); 
   }
-};
+}
+
+
+
+
+
+
+
 
 
 
